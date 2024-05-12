0cd351f1409005958b7f353eb16b578d5b31388b
xtrullor73 <Xtrullor@bk.ru>
2024-05-11 19:25:06 -0700

    refactor: move checkEnvVariables and checkInputPath to services directory

---
+++ src/services/checkEnvVariables.js
@@ -0,0 +1,5 @@
+export function checkEnvVariables() {
+  if (!process.env.ACOUSTID_API_KEY || !process.env.SPOTIFY_CLIENT_ID || !process.env.SPOTIFY_CLIENT_SECRET) {
+    throw new Error('Please set up ACOUSTID_API_KEY, SPOTIFY_CLIENT_ID, and SPOTIFY_CLIENT_SECRET in your .env file.');
+  }
+}
