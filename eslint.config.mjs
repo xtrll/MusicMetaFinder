683a945ddeb00f48b614ba26a45d35231e3b3c40
xtrullor73 <Xtrullor@bk.ru>
2024-05-25 00:19:31 -0700

    feat(eslint-config): extend ruleset with custom linting exceptions and adjustments

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,17 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended});
 
+// Add additional language options and rules here
+const additionalConfig = {
+  rules: {
+    'no-console': 'off', // This disables the 'no-console' rule
+    'import/no-named-as-default-member': 'off', // Disables the named as default member rule
+    'max-len': ['error', { code: 100, ignoreComments: true }], // Allow code lines of up to 100 characters, ignoring comments
+  },
+};
+
 export default [
-  {languageOptions: { globals: globals.browser }},
+  { languageOptions: { globals: globals.browser }},
   ...compat.extends("airbnb-base"),
+  additionalConfig,
 ];
